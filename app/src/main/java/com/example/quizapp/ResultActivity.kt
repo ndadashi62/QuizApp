3de16bf9f85af77cc0dd5f473dec8101fe119867
ndadashi62 <nasim.dadashi@gmail.com>
2021-01-19 12:44:01 -0500

    adding finish() to the intent of result Activity

--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -9,40 +9,14 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        val username=intent.getStringExtra(Constants.USER_NAME)
-        textViewUserName.text=username
-        val totalQuestion=intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
-        val correctAnswers=intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
-        textViewScore.text="your score is $correctAnswers out of $totalQuestion"
+        val username = intent.getStringExtra(Constants.USER_NAME)
+        textViewUserName.text = username
+        val totalQuestion = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        textViewScore.text = "your score is $correctAnswers out of $totalQuestion"
         btn_finish.setOnClickListener {
-            startActivity(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
 
 
