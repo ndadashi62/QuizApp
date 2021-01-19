3ff6e3a1b74882af228c050d72d56f7cf60997c8
ndadashi62 <nasim.dadashi@gmail.com>
2021-01-18 22:45:04 -0500

    we recive intent information in ReultActivity

--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,11 +1,23 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val username=intent.getStringExtra(Constants.USER_NAME)
+        textViewUserName.text=username
+        val totalQuestion=intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
+        val correctAnswers=intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
+        textViewScore.text="your score is $correctAnswers out of $totalQuestion"
+        btn_finish.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+
     }
 }
